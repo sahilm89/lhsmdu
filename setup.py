278ff97a938721201a24495da62ce63235acb976
sahil <sahilm@ncbs.res.in>
2020-05-26 11:54:06 +0530

    Corrected version number in config file

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ def readme():
         return f.read()
 
 setup(name='lhsmdu',
-      version='0.1',
+      version='1.1',
       description='This is an implementation of Latin Hypercube Sampling with Multi-Dimensional Uniformity (LHS-MDU) from Deutsch and Deutsch, "Latin hypercube sampling with multidimensional uniformity.',
       long_description=readme(),
       url='http://github.com/sahilm89/lhsmdu',
